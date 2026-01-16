bc66db3cb79022b3cbbe4679ddb321b0cc55d90a
kareemhamed001 <khidmt55@gmail.com>
2026-01-16 16:49:40 +0200

    add JWTTokenDuration to Config struct and initialize in NewConfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,21 +4,23 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	AppPort       int
-	AppEnv        string
-	DBDriver      string
-	DBHost        string
-	DBPort        string
-	DBUser        string
-	DBPassword    string
-	DBName        string
-	MongoDBUri    string
-	JWTPrivateKey string
+	AppPort          int
+	AppEnv           string
+	DBDriver         string
+	DBHost           string
+	DBPort           string
+	DBUser           string
+	DBPassword       string
+	DBName           string
+	MongoDBUri       string
+	JWTPrivateKey    string
+	JWTTokenDuration time.Duration
 }
 
 func NewConfig() *Config {
@@ -27,16 +29,17 @@ func NewConfig() *Config {
 	}
 
 	return &Config{
-		AppPort:       getEnvInt("APP_PORT", 8080),
-		AppEnv:        getEnvString("APP_ENV", "local"),
-		DBDriver:      getEnvString("DB_DRIVER", "postgres"),
-		DBHost:        getEnvString("DB_HOST", "localhost"),
-		DBPort:        getEnvString("DB_PORT", "5432"),
-		DBUser:        getEnvString("DB_USER", "admin"),
-		DBPassword:    getEnvString("DB_PASSWORD", "admin"),
-		DBName:        getEnvString("DB_NAME", "faq_db"),
-		MongoDBUri:    getEnvString("MONGO_DB_URI", "mongodb://admin:admin@localhost:27017/faq_db"),
-		JWTPrivateKey: getEnvString("JWT_PRIVATE_KEY", "your_jwt_private_key"),
+		AppPort:          getEnvInt("APP_PORT", 8080),
+		AppEnv:           getEnvString("APP_ENV", "local"),
+		DBDriver:         getEnvString("DB_DRIVER", "postgres"),
+		DBHost:           getEnvString("DB_HOST", "localhost"),
+		DBPort:           getEnvString("DB_PORT", "5432"),
+		DBUser:           getEnvString("DB_USER", "admin"),
+		DBPassword:       getEnvString("DB_PASSWORD", "admin"),
+		DBName:           getEnvString("DB_NAME", "faq_db"),
+		MongoDBUri:       getEnvString("MONGO_DB_URI", "mongodb://admin:admin@localhost:27017/faq_db"),
+		JWTPrivateKey:    getEnvString("JWT_PRIVATE_KEY", "your_jwt_private_key"),
+		JWTTokenDuration: time.Duration(getEnvInt("JWT_TOKEN_DURATION", 24)) * time.Hour,
 	}
 }
 
